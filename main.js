90ec6fb454da838c4f4e6b73fbcdd7b5ef519c16
dinine <letbegin201@gmail.com>
2022-12-12 01:57:19 +0700

    third

--- main.js
+++ main.js
@@ -12,3 +12,35 @@ function carousel() {
     x[myIndex - 1].style.display = "block";
     setTimeout(carousel, 2000);
 }
+
+var postApi = 'https://disease.sh/v3/covid-19/countries/vn'
+
+fetch(postApi)
+    .then(function (response) {
+        return response.json()
+
+    })
+    .then(function (news) {
+        console.log(news.countryInfo.flag)
+        var cases = news.cases
+        var deaths = news.deaths
+        var todayCases = news.todayCases
+        var todayDeaths = news.todayDeaths
+        var recovered = news.recovered
+        var todayRecovered = news.todayRecovered
+        var flagg = news.countryInfo.flag
+        document.getElementById('item cases').innerHTML = "Tổng số ca : " + cases
+        document.getElementById('item deaths').innerHTML = "Tổng số thiệt mạng : " + deaths
+        document.getElementById('item todayCases').innerHTML = "Số ca mới trong ngày : " + todayCases
+        document.getElementById('item todayDeaths').innerHTML = "Số thiệt mạng trong ngày : " + todayDeaths
+        document.getElementById('item recovered').innerHTML = "Tổng số phục hồi : " + recovered
+        document.getElementById('item todayRecovered').innerHTML = "Số phục hồi trong ngày : " + todayRecovered
+        document.getElementById("flag").src = flagg
+    })
+
+
+n = new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("DateTime").innerHTML = "Ngày: " + d + ", Tháng: " + m + ", Năm: " + y;
